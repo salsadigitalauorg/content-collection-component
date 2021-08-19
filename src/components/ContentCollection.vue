b08d4c6cdfe18706e812ecdb40238ae6e6ebeeb2
Alan <alan@salsadigital.com.au>
2021-08-19 11:37:10 +1000

    Add lock file and notice.

--- src/components/ContentCollection.vue
+++ src/components/ContentCollection.vue
@@ -48,6 +48,7 @@ export default {
   },
   computed: {
     dataManager () {
+      // TODO: This has changed slightly from the default implementation.
       return new ContentCollection(this.schema)
     },
     title () {
