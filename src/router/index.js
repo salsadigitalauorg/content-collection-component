a4e889bf3dd8537aa0cfeeb76469cf0786e23635
Nicolas Haase <nicolas.haase@gmail.com>
2021-09-02 14:58:45 +1000

    Added health alerts component for more testing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import HealthAlerts from '../views/HealthAlerts.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/health-alerts',
+    name: 'Health alerts',
+    component: HealthAlerts
   }
 ]
 
