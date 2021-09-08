7232b8ffcc8174a03a34f3e2635fe9cf6357fd5a
Alan <alan@salsadigital.com.au>
2021-09-08 14:31:05 +1000

    Fix for hosting.

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  runtimeCompiler: true,
+  filenameHashing: false,
+  productionSourceMap: false,
+  devServer: {
+    disableHostCheck: true
+  },
+  publicPath: '/content-collection-component/'
+}
